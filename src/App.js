558c55125717ade70d73827af528381c1647ba00
seventhlegion <93672714+seventhlegion@users.noreply.github.com>
2022-06-24 01:12:36 +0430

    Add files via upload
    responsive fixed with css grid and css flexbox

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css';
 import logo from '../src/pic/warning.png'
 import { useForm } from "react-hook-form";
-import { useState } from 'react';
 import { Month } from '../src/units/month';
 import { Day } from '../src/units/Day';
 import { Year } from '../src/units/year'
@@ -12,7 +11,14 @@ function App() {
 
   const {register, handleSubmit, formState: { errors }} = useForm();
 
+  const [data, setData] = useState({});
+
+  const [userName, setUserName] = useState('');
+
+  const [email, setEmail] = useState('');
+
   const [pass, setPass] = useState('');
+
   const [confirm, setConfirm] = useState('');
 
   const [day, setDay] = useState('Day');
@@ -34,28 +40,35 @@ function App() {
   }
 
   const onSubmit = (e) => {
-    console.log(e)
     axios.post('https://my-json-server.typicode.com/typicode/demo/posts', e)
     .then(response => {
-      console.log(response);
+      setData(response.data);
     })
     .catch(error => {
       console.log(error);
     })
+    console.log(e);
   }
+
+  console.log(data)
+
+  useEffect(() => {
+    onSubmit()
+  }, [])
 
   return (
     <>
     <div className='main'>
       <div className='container'>
       <input className='close' type={'button'} value={'X'}/>
-      <p>Sign up</p>
+      <p className='title'>Sign up</p>
         <form onSubmit={handleSubmit(onSubmit)}>
           <label className='label'>Username: </label>
           <br/>
           {errors.username && <img src={logo} alt={''} className={'error'}/>}
           <input 
             autoFocus={true}
+            onChange={(e) => {setUserName(e.target.value())}}
             className={errors.username? 'input-error': 'input'}
             type={'text'} 
             placeholder={'Enter your username'}
@@ -77,6 +90,7 @@ function App() {
           <br/>
           {errors.email && <img src={logo} alt={''} className={'error'}/>}
           <input
+            onChange={(e) => {setEmail(e.target.value())}}
             className={errors.email? 'input-error': 'input'}
             type={'text'} 
             placeholder={'example@gamil.com'}
@@ -133,8 +147,8 @@ function App() {
             <tr>
               <td>
                 <select
-                 className={errors.month? 'month-error': 'month'}
-                 {...register('month', {validate: val => val !== 'Month' || 'Select your month'})}
+                 className={errors.month? 'error-brith': 'birth'}
+                 {...register('month', {validate: val => val !== 'Month' || 'Select your birth day'})}
                  onChange={(e) => {setMoon(e.target.value)}}
                 >
                 <option value={'Month'}>Month</option>
@@ -144,7 +158,7 @@ function App() {
               <td>
                 <select
                   className={errors.day? 'error-brith': 'birth'}
-                  {...register('day', {validate: val => val !== 'Day' || 'Select your day'})}
+                  {...register('day', {validate: val => val !== 'Day' || 'Select your birth day'})}
                   onChange={(e) => {setDay(e.target.value)}}
                 >
                 <option value={'Day'}>Day</option>
@@ -154,7 +168,7 @@ function App() {
               <td>
                 <select
                   className={errors.year? 'error-brith': 'birth'}
-                  {...register('year', {validate: val => val !== 'Year' || 'Select your Year'})}
+                  {...register('year', {validate: val => val !== 'Year' || 'Select your birth day'})}
                   onChange={(e) => {setYear(e.target.value)}}
                 >
                 <option value={'Year'}>Year</option>
@@ -165,11 +179,8 @@ function App() {
             </tr>
           </table>
           <ul>
-            {errors.month && <li className='p-birth-error'>{errors.month.message}</li>}
-            {errors.day && <li className='p-birth-error'>{errors.day.message}</li>}
-            {errors.year && <li className='p-birth-error'>{errors.year.message}</li>}
+            {errors.month && <li className='p-birth-error'>{errors.month.message || errors.day.message || errors.year.message}</li>}
           </ul>
-          <br/>
           <input className='btn' type={'submit'} value={'Submit'} />
         </form>
       </div>
